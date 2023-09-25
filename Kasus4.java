874d14c715282e7e816bf8779197d8ebac2016ec
GalantPratama <145634582+GalantPratama@users.noreply.github.com>
2023-09-25 10:51:06 +0700

    Add files via upload

---
+++ Kasus4.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Galant
+ */
+public class Kasus4 {
+    public static void main(String[] args) {
+        // Deklarasi variabel sesuai dengan tipe data dan inisialisasi
+        int number = 10;
+        char letter = 'a';
+        boolean result = true;
+        String str = "hello";
+        
+        // Menampilkan hasil output
+        System.out.println("Number = " + number);
+        System.out.println("letter = " + letter);
+        System.out.println("result = " + result);
+        System.out.println("str = " + str);
+    }
+}
+
